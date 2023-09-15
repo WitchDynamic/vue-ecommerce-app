29bce121a29bd8205b34cc2fab050187a87dba33
Jacqueline Vargas <vargasjacqueline96@gmail.com>
2023-09-15 00:08:33 -0700

    basic routing

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from "vue";
+import ProductsPage from "./components/ProductsPage.vue";
+import CartPage from "./components/CartPage.vue";
+
+const routes = {
+  "/": ProductsPage,
+  "/cart": CartPage,
+};
+
+const currentPath = ref(window.location.hash || "#/");
+
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || "/"];
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <a href="#/">Products</a>
+  <a href="#/cart">Cart</a>
+  <component :is="currentView" />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
