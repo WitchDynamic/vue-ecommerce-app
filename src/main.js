c4135595a853dc897dff6e7a9ba7d03e70d1c169
Jacqueline Vargas <vargasjacqueline96@gmail.com>
2023-09-14 23:31:46 -0700

    setting up bootstrap

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "./assets/main.css";
 
-// Import Bootstrap and BootstrapVue CSS files
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-
-import { createApp, use } from "vue";
+import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
 const app = createApp(App);
-
-// Make BootstrapVue available throughout project
-use(BootstrapVue);
-use(IconsPlugin);
 
 app.use(createPinia());
 
