6d75805094230cc5f65fdb536c0f158d0822c995
Jacqueline Vargas <vargasjacqueline96@gmail.com>
2023-09-16 18:26:59 -0700

    created add/minus counter for quantity amount

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -1,10 +1,19 @@
 <script setup>
+import QuantityButton from "./QuantityButton.vue";
+import { ref } from "vue";
 import { useCartStore } from "@/stores/cart";
 const cartStore = useCartStore();
 
 const props = defineProps({
   product: Object,
 });
+// elevate quantity state to parent to pass to QuantityButton and Add To Cart button
+const quantity = ref(0);
+
+function updateQuantity(val) {
+  // do not allow value to go below 0
+  quantity.value + val < 0 ? (quantity.value = 0) : (quantity.value += val);
+}
 </script>
 
 <template>
@@ -13,8 +22,14 @@ const props = defineProps({
     <div class="card-body">
       <h5 class="card-text">{{ product.name }}</h5>
       <p class="card-text">{{ product.price }} Credits</p>
+      <QuantityButton :quantity="quantity" @updateQuantity="updateQuantity" />
       <Button
-        @click="cartStore.addItems(5, product)"
+        @click="
+          () => {
+            cartStore.addItems(quantity, product);
+            quantity = 0;
+          }
+        "
         class="btn btn-primary card-btn"
         >Add To Cart</Button
       >
---
+++ src/components/QuantityButton.vue
@@ -0,0 +1,40 @@
+<script setup>
+// ProductItem parent handles quantity
+// minus and plus buttons emit event to update quantity value
+const props = defineProps({
+  quantity: Number,
+});
+</script>
+
+<template>
+  <div class="wrapper">
+    <button
+      @click="$emit('updateQuantity', -1)"
+      class="btn btn-outline-secondary"
+      type="button"
+    >
+      -
+    </button>
+
+    <input type="text" class="form-control input-box" :value="quantity" />
+    <button
+      @click="$emit('updateQuantity', 1)"
+      class="btn btn-outline-secondary"
+      type="button"
+    >
+      +
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  margin: 1rem;
+}
+.input-box {
+  /* width: 50px; */
+  text-align: center;
+  cursor: pointer;
+}
+</style>
