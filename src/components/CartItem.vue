7280cccf0f28518470ffc4e16b797d8841a5947e
Jacqueline Vargas <vargasjacqueline96@gmail.com>
2023-09-16 22:10:51 -0700

    html for cart page

---
+++ src/components/CartItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { useCartStore } from "@/stores/cart";
+const cartStore = useCartStore();
+</script>
+
+<template>
+  <div style="display: flex">
+    <img
+      :src="cartStore.products[0].image"
+      width="110"
+      height="110"
+      style="border-radius: 10px"
+    />
+    <div class="product-info">
+      <h4>{{ cartStore.products[0].name }}</h4>
+      <p>Quantity: 2</p>
+      <Button class="trash-button">
+        <img src="@/assets/trash-icon.svg" width="25" height="25" />
+      </Button>
+    </div>
+    <div class="product-price">
+      <h5 style="color: #fff">Credits</h5>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.product-info {
+  color: white;
+  margin-left: 1rem;
+}
+.trash-button {
+  background: transparent;
+  border: none;
+}
+.product-price {
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  margin-right: 0;
+}
+</style>
--- src/components/CartPage.vue
+++ src/components/CartPage.vue
@@ -1,7 +1,39 @@
+<script setup>
+import CartItem from "./CartItem.vue";
+</script>
 <template>
-  <div><h1>Cart Page</h1></div>
+  <h1 class="title">Your Cart</h1>
+  <div class="container">
+    <div class="row align-items-start">
+      <div class="col-8">
+        <CartItem />
+      </div>
+      <div class="col-4">
+        <div class="total-price-div">
+          <h2>Total Price:</h2>
+          <h3>10500 Credits</h3>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<style scoped>
+.title {
+  text-align: center;
+  padding-bottom: 2rem;
+  color: #e2e1e1;
+}
 
-<style></style>
+.cart-grid {
+  justify-content: center;
+  display: flex;
+  padding: 10px;
+}
+.total-price-div {
+  color: #e2e1e1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
